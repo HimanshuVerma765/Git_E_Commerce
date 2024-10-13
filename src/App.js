9b8c6859f15f7b922006892d1e6704ef2856df6f
harendra <jeaturajabundela@email.com>
2024-10-13 15:04:25 +0530

    dashbord with logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Dashboard from './pages/home';
+import Header from './components/Header';
+
 
 function App() {
   return (
     <>
-    <h1 className='text-danger' >Hello EveryOne!</h1>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/dashboard" exact = {true} element={<Dashboard/>} />
+      <Route path="/" exact = {true} element={<Dashboard/>} />
+    </Routes>
+    </BrowserRouter>
     </>
   );
 }
