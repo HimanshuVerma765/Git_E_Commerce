11a90f4ed9601173a7b6c45edd2106bd4632a794
HimanshuVerma765 <himanshuverma4765@gmail.com>
2024-10-13 15:06:47 +0530

    some work of header is done

---
+++ src/Components/Header/index.js
@@ -0,0 +1,40 @@
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/images/logo.png'
+
+const Header = () => {
+    return (
+        <>
+            <div className="headerWrapper">
+                <div className="top-strip bg-blue">
+                    <div className="container">
+                        <p className="mb-0 mt-0 text-center">Due to the <b>COVID 19</b> epidemic, orders may be processed with a slight delay</p>
+                    </div>
+                </div>
+
+                <div className="header">
+                    <div className="container">
+                        <div className="row"> 
+                            <div className="logoWrapper d-flex align-items-center col-sm-2"> 
+                            <Link to={'/'}> <img src={Logo}></img></Link>
+                            </div>
+
+                            <div className='col-sm-10 d-flex align-items-center part2'>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+
+
+            </div>
+        </>
+    )
+
+}
+
+export default Header;
