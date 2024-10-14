15bfb0a675088d73486d13a9c745c778638cf57b
HimanshuVerma765 <himanshuverma4765@gmail.com>
2024-10-14 16:56:09 +0530

    finally header is commplete now

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -3,7 +3,8 @@ import Logo from '../../assets/images/logo.png'
 import CountryDropdown from '../CountryDropdown';
 import { IoMdSearch } from "react-icons/io";
 import { Button } from '@mui/material';
-
+import { LuUser2 } from "react-icons/lu";
+import { TbShoppingBag } from "react-icons/tb";
 const Header = () => {
     return (
         <>
@@ -14,25 +15,37 @@ const Header = () => {
                     </div>
                 </div>
 
-                <div className="header">
+                <header className="header">
                     <div className="container">
                         <div className="row">
                             <div className="logoWrapper d-flex align-items-center col-sm-2">
                                 <Link to={'/'}> <img src={Logo} alt='Logo'></img></Link>
                             </div>
                             <div className='col-sm-10 d-flex align-items-center part2'>
-                                <CountryDropdown/>
+                                <CountryDropdown />
 
                                 {/*Header Search start here*/}
-                                <div className='headerSearch'>
-                                    <input type='text'/>
-                                    <Button> <IoMdSearch/></Button>
+                                <div className='headerSearch ml-3 mr-3'>
+                                    <input type='text' placeholder='Search for products...' />
+                                    <Button> <IoMdSearch /></Button>
                                 </div>
                                 {/*Header Search ends here*/}
+
+                                <div className='part3 d-flex align-items-center ml-auto'>
+                                    <Button className='circle mr-3'><LuUser2 /></Button>
+                                    <div className='cartTab ml-auto d-flex align-items-center'>
+                                        <span className='price' >$3.29</span>
+                                        <div className='position-relative ml-2'>
+                                        <Button className='circle'><TbShoppingBag /></Button>
+                                        <span className='count d-flex align-items-center justify-content-center'>1</span>
+                                        </div>
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
                     </div>
-                </div>
+                </header>
 
 
 
